37f2bbbea8212feb21b12973d611089c21290395
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-01-23 15:13:42 -0500

    favIconAdded

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ document.addEventListener('DOMContentLoaded', function(){
     const workInput = document.querySelector('#work')
     const buttons = document.querySelector('#buttons')
 
+    const changeFavicon = link => {
+        let $favicon = document.querySelector('link[rel="icon"]')
+        // If a <link rel="icon"> element already exists,
+        // change its href to the given link.
+        if ($favicon !== null) {
+          $favicon.href = link
+        // Otherwise, create a new element and append it to <head>.
+        } else {
+          $favicon = document.createElement("link")
+          $favicon.rel = "icon"
+          $favicon.href = link
+          document.head.appendChild($favicon)
+        }
+      }
+
+    changeFavicon("https://i.pinimg.com/originals/84/0a/50/840a50493a532c3bc3c512486fc31ac7.png")
+
+
     demoInput.style.color = '#5E6472'
 
     buttons.addEventListener('click', (e) => {
