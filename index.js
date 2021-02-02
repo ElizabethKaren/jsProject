dfa036e002328b22e9edc07b29692b1cb877c54f
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-02-02 11:10:46 -0500

    faviconMethodAdded

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelector('#buttons')
 
     const changeFavicon = link => {
-        let $favicon = document.querySelector('link[rel="icon"]')
-        // If a <link rel="icon"> element already exists,
-        // change its href to the given link.
-        if ($favicon !== null) {
-          $favicon.href = link
-        // Otherwise, create a new element and append it to <head>.
+        let favicon = document.querySelector('link[rel="icon"]')
+        if (favicon !== null) {
+          favicon.href = link
         } else {
-          $favicon = document.createElement("link")
-          $favicon.rel = "icon"
-          $favicon.href = link
-          document.head.appendChild($favicon)
+          favicon = document.createElement("link")
+          favicon.rel = "icon"
+          favicon.href = link
+          document.head.appendChild(favicon)
         }
       }
 
